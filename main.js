9bd5228a43448d79174e8c0fb04b2e4c3a2ad30a
Andy Zhang <yd.zhang.92@gmail.com>
2021-02-05 23:25:16 -0500

    use library

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const {
 	API_SECRET
 } = require("./secrets.js")
 
-function main() {
-	console.log(API_KEY);
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: API_KEY,
+  APISECRET: API_SECRET
+});
+
+
+async function main() {
+	coin = process.argv[2]
+	let ticker = await binance.prices();
+	console.info(`Price of ${coin}: ${ticker[coin]}`);
 }
 
 main();
